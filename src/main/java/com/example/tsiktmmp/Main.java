0d9dd0789e7924662311c33f9d095bed610d808e
Mykola <xmelnyk@stuba.sk>
2023-11-28 17:06:41 +0100

    Fixed few bugs

--- src/main/java/com/example/tsiktmmp/Main.java
+++ src/main/java/com/example/tsiktmmp/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 320, 320);
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
